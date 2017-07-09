ce2c339a7b366b548fc53132643434feb84818c5
RoguePotato <anthony.mercer@hotmail.co.uk>
2017-07-09 13:15:44 +0100

    added doxygen config

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+/// @file Main.cpp
+/// @brief this header file will contain all required
+/// definitions and basic utilities functions.
+//
+/// @author Anthony Mercer
+//
+/// @date 07/07/2017
+
+#include <stdio>
+
+/// Main function does this that and the other
+int main() {
+    printf("Hello world\n");
+    return 0;
+}
