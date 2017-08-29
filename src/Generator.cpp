1bbd12c8f1fff1118ea59603d68d1db40c4c7972
RoguePotato <anthony.mercer@hotmail.co.uk>
2017-08-29 15:42:33 +0100

    Tab formatting

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -84,42 +84,42 @@ void Generator::CreateDisc(void) {
 
     real64 index[2];
     index[0] = 1.0f - (mP / 2.0f);
-		index[1] = 2.0f / (2.0f - mP);
+    index[1] = 2.0f / (2.0f - mP);
 
-		real64 omegaIn = pow(1.0 + mOmegaIn, index[0]);
-		real64 omegaOut = pow(1.0 + mOmegaOut, index[0]);
-		real64 inner = (omegaIn + mRands[0] * (omegaOut - omegaIn));
+    real64 omegaIn = pow(1.0 + mOmegaIn, index[0]);
+    real64 omegaOut = pow(1.0 + mOmegaOut, index[0]);
+    real64 inner = (omegaIn + mRands[0] * (omegaOut - omegaIn));
 
-		real64 omega = pow(inner, index[1]) - 1.0;
-		real64 R = mR0 * pow(omega, 0.5f);
-		real64 phi = 2 * PI * mRands[1];
+    real64 omega = pow(inner, index[1]) - 1.0;
+    real64 R = mR0 * pow(omega, 0.5f);
+    real64 phi = 2 * PI * mRands[1];
 
-		real64 x = R * cos(phi);
-		real64 y = R * sin(phi);
+    real64 x = R * cos(phi);
+    real64 y = R * sin(phi);
 
-		real64 sigma = mSigma0 *
+    real64 sigma = mSigma0 *
                    pow((mR0 * mR0) / (mR0 * mR0 + R * R), mP / 2);
 
     real64 T = pow(pow(mTinf, 4.0) + pow(mT0, 4.0) *
                pow(pow(R, 2.0) + pow(mR0, 2.0), -2.0 * mQ), 0.25);
-		real64 cS2 = ((K * T) / (MU * M_P)) / (AU_TO_M * AU_TO_M);
+    real64 cS2 = ((K * T) / (MU * M_P)) / (AU_TO_M * AU_TO_M);
 
-		real64 z_0 = -((PI * sigma * R * R * R) / (2 * mMStar)) +
+    real64 z_0 = -((PI * sigma * R * R * R) / (2 * mMStar)) +
                  pow(pow((PI * sigma * R * R * R) / (2 * mMStar), 2.0f) +
                  ((cS2 * R * R * R) / (G_AU * mMStar)), 0.5f);
 
-		real64 z = (2 / PI) * z_0 * asin(2 * mRands[2] - 1);
+    real64 z = (2 / PI) * z_0 * asin(2 * mRands[2] - 1);
 
-		real64 rho = ((PI * mSigma0) / (4 * z_0)) *
+    real64 rho = ((PI * mSigma0) / (4 * z_0)) *
                  pow((mR0 * mR0) / (mR0 * mR0 + R * R), 0.5) *
                  cos((PI * z) / (2 * z_0));
 
-		real64 m = mMDisc / mNumHydro;
+    real64 m = mMDisc / mNumHydro;
 
-		real64 h = pow((3 * mNumNeigh * m) / (32 * PI * rho), (1.0f / 3.0f));
+    real64 h = pow((3 * mNumNeigh * m) / (32 * PI * rho), (1.0f / 3.0f));
 
     // Set U later from OpacityTable
-		real64 U = (K * T) / (MU * M_P * (GAMMA - 1.0f));
+    real64 U = (K * T) / (MU * M_P * (GAMMA - 1.0f));
 
     mParticles.at(i)->SetID(i);
     mParticles.at(i)->SetX(Vec3(x, y, z));
@@ -141,48 +141,48 @@ void Generator::CreateStars(void) {
   p->SetH(1.0);
   p->SetM(mMStar);
   p->SetType(-1);
-	mParticles.push_back(p);
+  mParticles.push_back(p);
 }
 
 void Generator::CalculateVelocity(void) {
-	real64 shift = mRout * 2.0;
-	mOctree = new Octree(Vec3(0.0, 0.0, 0.0), Vec3(5000.0, 5000.0, 5000.0));
+  real64 shift = mRout * 2.0;
+  mOctree = new Octree(Vec3(0.0, 0.0, 0.0), Vec3(5000.0, 5000.0, 5000.0));
 
-	mOctreePoints = new OctreePoint[mParticles.size()];
-	for(int i = 0; i < mParticles.size(); ++i) {
-		real64 x = mParticles.at(i)->GetX()[0];
-		real64 y = mParticles.at(i)->GetX()[1];
-		real64 z = mParticles.at(i)->GetX()[2];
-		real64 M = mParticles.at(i)->GetM();
+  mOctreePoints = new OctreePoint[mParticles.size()];
+  for(int i = 0; i < mParticles.size(); ++i) {
+    real64 x = mParticles.at(i)->GetX()[0];
+    real64 y = mParticles.at(i)->GetX()[1];
+    real64 z = mParticles.at(i)->GetX()[2];
+    real64 M = mParticles.at(i)->GetM();
 
-		// shift the positions such that they are positive
-		x += shift;
-		y += shift;
-		z += shift;
+    // shift the positions such that they are positive
+    x += shift;
+    y += shift;
+    z += shift;
 
-		mOctreePoints[i].SetPosition(Vec3(x, y, z));
-		mOctreePoints[i].SetMass(M);
-	 	mOctree->Insert(mOctreePoints + i);
-	}
+    mOctreePoints[i].SetPosition(Vec3(x, y, z));
+    mOctreePoints[i].SetMass(M);
+    mOctree->Insert(mOctreePoints + i);
+  }
 
-	for (int i = 0; i < mParticles.size(); ++i) {
-		Vec3 acc = Vec3(0.0, 0.0, 0.0);
-		Vec3 pos = mParticles.at(i)->GetX();
-		real64 R = mParticles.at(i)->GetR();
-		real64 x = mParticles.at(i)->GetX()[0];
-		real64 y = mParticles.at(i)->GetX()[1];
-		real64 h = mParticles.at(i)->GetH();
+  for (int i = 0; i < mParticles.size(); ++i) {
+    Vec3 acc = Vec3(0.0, 0.0, 0.0);
+    Vec3 pos = mParticles.at(i)->GetX();
+    real64 R = mParticles.at(i)->GetR();
+    real64 x = mParticles.at(i)->GetX()[0];
+    real64 y = mParticles.at(i)->GetX()[1];
+    real64 h = mParticles.at(i)->GetH();
 
-		pos[0] += shift;
-		pos[1] += shift;
-		pos[2] += shift;
+    pos[0] += shift;
+    pos[1] += shift;
+    pos[2] += shift;
 
-		mOctree->TraverseTree(pos, acc, h);
+    mOctree->TraverseTree(pos, acc, h);
 
-		real64 v = sqrt(acc.Norm() * R) * AU_TO_KM;
-		real64 vX = (-v * y) / (R + 0.000001);
-		real64 vY = (v * x) / (R + 0.000001);
+    real64 v = sqrt(acc.Norm() * R) * AU_TO_KM;
+    real64 vX = (-v * y) / (R + 0.000001);
+    real64 vY = (v * x) / (R + 0.000001);
 
-		mParticles.at(i)->SetV(Vec3(vX, vY, 0.0));
-	}
+    mParticles.at(i)->SetV(Vec3(vX, vY, 0.0));
+  }
 }
